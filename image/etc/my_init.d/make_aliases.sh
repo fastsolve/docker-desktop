75d16cde8d0947ef35b01fa4e0d49e8c2818f455
Xiangmin Jiao <xmjiao@gmail.com>
2017-07-05 09:42:16 -0400

    Changed make_aliases

--- image/etc/my_init.d/make_aliases.sh
+++ image/etc/my_init.d/make_aliases.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [[ -n "$MATLAB_VERSION" ]]; then
+if [ -n "$MATLAB_VERSION" ]; then
   ln -s -f /usr/local/MATLAB/$MATLAB_VERSION/bin/matlab /usr/local/bin
   ln -s -f /usr/local/MATLAB/$MATLAB_VERSION/bin/mex /usr/local/bin
   ln -s -f /usr/local/MATLAB/$MATLAB_VERSION/bin/glnxa64/mlint /usr/local/bin
+elif [ -e  /usr/local/mlint/bin/glnxa64/mlint ]; then
+  ln -s -f /usr/local/mlint/bin/glnxa64/mlint /usr/local/bin
 fi
